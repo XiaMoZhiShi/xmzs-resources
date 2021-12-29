04e643e5463f97939b312a04d4b01d89a7420cd4
MATRIX-feather <midnightcarnival@outlook.com>
2021-12-29 10:44:51 +0800

    enchance: 使用PackSquash压缩和优化资源包 * misc: 修复pack.png是jpeg格式的问题 * misc: 只在发布release和有新pr出现时跑workflows
    Signed-off-by: MATRIX-feather <midnightcarnival@outlook.com>

--- tools/makepackage.sh
+++ tools/makepackage.sh
@@ -3,6 +3,15 @@ set -e
 
 readonly BUILD_DIR="$PWD/build";
 
+readonly PACKSQUASH_OPTIONS="
+pack_directory = \"$PWD\"
+output_file_path = \"$PWD/build/xmzs-resources.zip\"
+
+#生成zip文件后就不要再允许以其他的方式更改压缩文档
+zip_spec_conformance_level = \"disregard\"
+size_increasing_zip_obfuscation = true
+"
+
 function changeDir()
 {
     local target="$1"
@@ -17,11 +26,7 @@ if [ ! -d "$BUILD_DIR" ];then
 fi
 
 echo "创建压缩文档..."
-zip -9 -r xmzs-resources.zip ./assets pack*
-zip -d xmzs-resources.zip "*.xcf"
-
-echo "移动文档..."
-mv xmzs-resources.zip build
+echo "$PACKSQUASH_OPTIONS" | tools/packsquash
 
 echo "生成sha1..."
 changeDir "$BUILD_DIR"
